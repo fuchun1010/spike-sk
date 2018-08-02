97baa27e7b49deb4e9c83ccce18b03cdfb1c7d54
Tank <fuch@fullsample.com>
2018-08-02 14:25:44 +0800

    [fuchun] #Tech first run success

---
+++ src/main/java/com/tank/Boot.java
@@ -0,0 +1,16 @@
+package com.tank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fuchun
+ */
+@SpringBootApplication
+public class Boot {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Boot.class, args);
+  }
+
+}
